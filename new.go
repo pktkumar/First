73dd2b49e8f0bb9c672ba4e1bc0f0e97876fe495
pktkumar <39775618+pktkumar@users.noreply.github.com>
2025-07-08 21:39:24 -0500

    updated

--- new.go
+++ new.go
@@ -2,44 +2,21 @@ package main
 
 import (
 	"fmt"
-	//"io"
+	"io"
 	"net/http"
 )
 
 ///go mod init First
 ///go run .
 
-func handlerOne(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is server on port 8083")
-}
-func handlerTwo(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is server on port 8084")
-}
-
 func myNew() {
-	fmt.Println("in myNew")
-	resp, err := http.Get("http://localhost:8080/name?myName=Kumar")
-
-	if err != nil {
-		fmt.Printf("error reading reponse")
-		return
-	}
-
-	defer resp.Body.Close()
-
-	if err != nil {
-		fmt.Printf("error reading body response")
-		return
-	}
 
 	mux1 := http.NewServeMux()
 	mux1.HandleFunc("/", handlerOne)
 
 	go func() {
 		fmt.Println("in 40....")
-
 		http.ListenAndServe(":8083", mux1)
-
 	}()
 
 }
@@ -50,10 +27,36 @@ func myTwo() {
 	mux2.HandleFunc("/", handlerTwo)
 
 	go func() {
-
 		fmt.Println("in 57....")
-
 		http.ListenAndServe(":8084", mux2)
 	}()
 
+}
+
+func handlerOne(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "This is server on port 8083 <br>")
+
+	fmt.Println("in myNew")
+	resp, err := http.Get("http://localhost:8080/name?myName=Kumar")
+
+	if err != nil {
+		fmt.Printf("error reading reponse")
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Failed to read response body", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%s", body)
+
+}
+
+func handlerTwo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is server on port 8084")
 }
